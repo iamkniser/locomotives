39aa7a8fd7908ad65172b8f224e54e754ffecb5d
Nikita Krivosheyev <nik.krivosheyev@gmail.com>
2022-11-16 16:36:01 +0600

    map added

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,8 +1,56 @@
-import React from 'react';
+import {
+  GoogleMap, InfoWindow, Marker, useLoadScript,
+} from '@react-google-maps/api';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import './Map.css';
 
 function Map() {
+  const locomotives = useSelector((state) => state.locomotives.locomotives);
+  const [selectedLocomotive, setSelectedLocomotive] = useState(null);
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_API_KEY,
+  });
+  if (!isLoaded) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
   return (
-    <div>Map</div>
+    <GoogleMap
+      zoom={6}
+      center={{ lat: 51.153086, lng: 71.416181 }}
+      mapContainerClassName="map-container"
+    >
+      {locomotives.map((locomotive) => (
+        <Marker
+          key={locomotive.id}
+          position={{ lat: locomotive.coordinates[0], lng: locomotive.coordinates[1] }}
+          onClick={() => { setSelectedLocomotive(locomotive); }}
+        />
+      ))}
+
+      {selectedLocomotive
+        && (
+        <InfoWindow
+          position={{
+            lat: selectedLocomotive.coordinates[0],
+            lng: selectedLocomotive.coordinates[1],
+          }}
+          onCloseClick={() => { setSelectedLocomotive(null); }}
+        >
+          <div>
+            <h2>{selectedLocomotive.title}</h2>
+            <p>{`Серия: ${selectedLocomotive.series}`}</p>
+            <p>{`Кол-во секций: ${selectedLocomotive.sections}`}</p>
+            <p>{`Координаты: ${selectedLocomotive.coordinates}`}</p>
+          </div>
+        </InfoWindow>
+        )}
+    </GoogleMap>
+
   );
 }
 
