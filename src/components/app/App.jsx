ae0d80b6d2a4d91ddef185cd4529c805f2e7d94e
Nikita Krivosheyev <nik.krivosheyev@gmail.com>
2022-11-16 16:36:23 +0600

    app fix

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -5,19 +5,18 @@ import AddForm from '../addForm/AddForm';
 import LocomotivesList from '../locomotives/LocomotivesList';
 import Map from '../map/Map';
 import Navbar from '../Navbar';
-import './App.css';
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className="wrapper">
-        <Routes>
-          <Route path="/locomotives-list" element={<LocomotivesList />} />
-          <Route path="/map" element={<Map />} />
-          <Route path="/locomotive" element={<AddForm />} />
-        </Routes>
-      </div>
+
+      <Routes>
+        <Route path="/locomotives-list" element={<LocomotivesList />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/locomotive" element={<AddForm />} />
+      </Routes>
+
     </>
   );
 }
